61c38218bb82f4065bd9f9364af8594f0c1e4a1d
Ralph Bean <ralph.bean@gmail.com>
2010-08-30 13:36:26 -0400

    Got basic template biz working.

--- ansi2html/ansi2html.py
+++ ansi2html/ansi2html.py
@@ -7,16 +7,37 @@
 # Python author:
 #    http://github.com/ralphbean/ansi2html/
 
-from genshi.template import MarkupTemplate
+from genshi.template import TemplateLoader, loader
 
-class AnsiToHTMLConverter(object):
+class Ansi2HTMLConverter(object):
     def __init__(self, template='ansi2html.templates.default'):
-        self.template = template
-        self.attrs = {}
-    
+        self._template = template
+        self._attrs = None
+
+    def prepare(self, ansi):
+        """ Load the contents of 'ansi' into this object """
+        self._attrs = {}
+        return self._attrs
+
+    def attrs(self):
+        """ Prepare attributes for the template """
+        if not self._attrs:
+            raise Exception, "Method .prepare not yet called."
+        return self._attrs
+
+    def template(self):
+        """ Load the template """
+        toks = self._template.split('.')
+        name, path, fname = toks[0], "/".join(toks[1:-1]), toks[-1] + '.html'
+        tmpl = TemplateLoader([loader.package(name, path)]).load(fname)
+        return tmpl
+
     def convert(self, ansi):
-        self.prepare
-        tmpl = MarkupTemplate(self.template)
-        html = tmpl.generate(**self.attrs).render('html')
-        return html
+        return self.template().generate(**self.prepare(ansi)).render('html')
+
+if __name__ == '__main__':
+    conv = Ansi2HTMLConverter()
+    html = conv.convert('foo')
+    print "done"
+    print html
 
