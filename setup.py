b62f733081b660fb59e14bd0e7b532b07e108eb9
Ralph Bean <ralph.bean@gmail.com>
2010-08-31 14:51:24 -0400

    updated version for pypi

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except ImportError:
 
 setup(
     name='ansi2html',
-    version='0.2',
+    version='0.2.1',
     description="Python Wrapper for pixelbeat.org's ansi2html.sh",
     long_description=open('README.md').read(),
     author='Ralph Bean',
