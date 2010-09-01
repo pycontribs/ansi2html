2000f3f29b87e6e52eda24d233d22453eec68806
Ralph Bean <ralph.bean@gmail.com>
2010-09-01 01:49:09 -0400

    got post install hook working with script permissions.

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+
+
 try:
     from setuptools import setup, find_packages
 except ImportError:
@@ -7,18 +9,20 @@ except ImportError:
     from setuptools import setup, find_packages
 
 from setuptools.command.install import install as _install
+import os
+import stat as s
 
 class install(_install):
     def run(self):
         _install.run(self)
-        print "*" * 78
-        print "Just a friendly post-install message."
-        print "*" * 78
+        script = "%s/ansi2html/ansi2html.sh" % self.install_libbase 
+        mode = s.S_IRUSR | s.S_IWUSR | s.S_IXUSR | s.S_IROTH | s.S_IXOTH
+        os.chmod(script, mode)
 
 setup(
     cmdclass={'install': install},
     name='ansi2html',
-    version='0.2.7',
+    version='0.4',
     description="Python Wrapper for pixelbeat.org's ansi2html.sh",
     long_description=open('README.md').read(),
     author='Ralph Bean',
