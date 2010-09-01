87177269c367b81731cabcb8f5575d9d342c5516
Ralph Bean <ralph.bean@gmail.com>
2010-08-31 22:58:30 -0400

    nevermind.  that was weird

--- ansi2html/ansi2html.py
+++ ansi2html/ansi2html.py
@@ -36,7 +36,7 @@ class Ansi2HTMLConverter(object):
         """ Load the contents of 'ansi' into this object """
 
         # For now, make heavy use of pixelbeat's amazing script.
-        cmd = ["/bin/sh", "%s/ansi2html.sh" % self.base]
+        cmd = ["%s/ansi2html.sh" % self.base]
         p = sp.Popen(cmd, stdout=sp.PIPE, stdin=sp.PIPE, shell=True)
         body = HTML(p.communicate(ansi)[0].decode('utf-8'))
 
