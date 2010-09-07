3a6d5ebfcc6998c1d808975bbf99208a7f139084
Ralph Bean <ralph.bean@gmail.com>
2010-09-06 21:38:02 -0400

    updated readme and pypi stuff

---
+++ README.rst
@@ -0,0 +1,21 @@
+ansi2html
+=========
+
+Convert text with ansi color codes to HTML
+------------------------------------------
+
+:Author: Ralph Bean <ralph.bean@gmail.com>
+
+.. _pixelbeaet: http://www.pixelbeat.org/docs/terminal_colours/
+.. _blackjack: http://www.koders.com/python/fid5D57DD37184B558819D0EE22FCFD67F53078B2A3.aspx
+
+Inspired by and developed off of the work of `pixelbeat`_ and `blackjack`_.
+
+
+Get this project:
+-----------------
+Source:  http://github.com/ralphbean/ansi2html/
+
+pypi:    http://pypi.python.org/pypi/ansi2html/
+
+
--- setup.py
+++ setup.py
@@ -8,23 +8,12 @@ except ImportError:
     use_setuptools()
     from setuptools import setup, find_packages
 
-from setuptools.command.install import install as _install
-import os
-import stat as s
-
-class install(_install):
-    def run(self):
-        _install.run(self)
-        script = "%s/ansi2html/ansi2html.sh" % self.install_libbase 
-        mode = s.S_IRUSR | s.S_IWUSR | s.S_IXUSR | s.S_IROTH | s.S_IXOTH
-        os.chmod(script, mode)
 
 setup(
-    cmdclass={'install': install},
     name='ansi2html',
-    version='0.4.4',
+    version='0.5.0',
     description="Python Wrapper for pixelbeat.org's ansi2html.sh",
-    long_description=open('README.md').read(),
+    long_description=open('README.rst').read(),
     author='Ralph Bean',
     author_email='ralph.bean@gmail.com',
     url='http://github.com/ralphbean/ansi2html/',
