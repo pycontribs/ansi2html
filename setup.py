6fc2b5953d4cf145757333d76b376ed5aebcf0fd
Ralph Bean <ralph.bean@gmail.com>
2011-11-27 21:48:52 -0500

    Version bump.

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ f.close()
 
 setup(
     name='ansi2html',
-    version='0.6.9',
+    version='0.7.0',
     description="Convert text with ANSI color codes to HTML",
     long_description=long_description,
     author='Ralph Bean',
