ed8a91d40c27f660af4003bc80f799880ce4c3ec
warp <kuno@frob.nl>
2012-01-04 06:28:35 -0500

    Add some simple unittests.

--- tests/test_ansi2html.py
+++ tests/test_ansi2html.py
@@ -9,7 +9,7 @@ _here = dirname (abspath (__file__))
 class TestAnsi2HTML (unittest.TestCase):
 
     def setUp(self):
-        with open (join (_here, "ansicolor"), "rb") as input:
+        with open (join (_here, "ansicolor.txt"), "rb") as input:
             self.input = "\n".join (input.readlines ())
 
         with open (join (_here, "ansicolor.html"), "rb") as output:
@@ -29,7 +29,11 @@ class TestAnsi2HTML (unittest.TestCase):
         conv = Ansi2HTMLConverter ()
         attrs = conv.prepare (cgi.escape (rainbow))
 
-        expected = u'<span class="ansi1"><span class="ansi40"><span class="ansi31">r<span class="ansi32">a<span class="ansi33">i<span class="ansi34">n<span class="ansi35">b<span class="ansi36">o<span class="ansi37">w</span>\n'
+        expected = (u'<span class="ansi1"><span class="ansi40">' +
+                    u'<span class="ansi31">r<span class="ansi32">a' +
+                    u'<span class="ansi33">i<span class="ansi34">n' +
+                    u'<span class="ansi35">b<span class="ansi36">o' +
+                    u'<span class="ansi37">w</span>\n')
         self.assertEqual (expected, attrs["body"])
         self.assertEqual ("normal", attrs["font_size"])
         self.assertTrue (attrs["dark_bg"])
