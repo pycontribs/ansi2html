7f5e2a0cdb1975de3800b34b8fcb80b8dccc8583
Ralph Bean <ralph.bean@gmail.com>
2012-01-04 20:30:40 -0500

    Removed vestigial template references.

--- ansi2html/ansi2html.py
+++ ansi2html/ansi2html.py
@@ -11,7 +11,6 @@
 ## along with this program; if not, write to the Free Software
 ## Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
-# from mako.template import Template
 import cgi
 import re
 import sys
