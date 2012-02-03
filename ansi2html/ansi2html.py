7597635260aef08f537583ad836236a57b1f1555
Ralph Bean <rbean@redhat.com>
2012-02-03 14:09:44 -0500

    License header formatting.

--- ansi2html/ansi2html.py
+++ ansi2html/ansi2html.py
@@ -1,15 +1,15 @@
-##Convert ANSI (terminal) colours and attributes to HTML
-##
-## Ralph Bean <rbean@redhat.com>
-##
-## Inspired by and developed off of the work by pixelbeat and blackjack.
-##
-## This software may be freely redistributed under the terms of the GNU
-## general public license.
-##
-## You should have received a copy of the GNU General Public License
-## along with this program; if not, write to the Free Software
-## Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+# Convert ANSI (terminal) colours and attributes to HTML
+#
+#  Ralph Bean <rbean@redhat.com>
+#
+#  Inspired by and developed off of the work by pixelbeat and blackjack.
+#
+#  This software may be freely redistributed under the terms of the GNU
+#  general public license.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
 import re
 import sys
