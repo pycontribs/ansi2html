pycontribs/ansi2html

commit 506015955ade7a9cebfa2458b2ca31300c572e5e

Unicode handling.

ansi2html/converter.py
@@ -305,5 +305,11 @@ def main():
         return
 
     # Otherwise, just process the whole thing in one go
-    output = conv.convert(" ".join(sys.stdin.readlines()))
-    _print(output)
+    if six.PY3:
+        output = conv.convert(" ".join(sys.stdin.readlines()))
+        _print(output)
+    else:
+        output = conv.convert(six.u(" ").join(
+            map(six.u, sys.stdin.readlines())
+        ))
+        _print(output.encode(opts.output_encoding))

tests/test_ansi2html.py
@@ -54,7 +54,12 @@ class TestAnsi2HTML(unittest.TestCase):
         with open(join(_here, "ansicolor.html"), "rb") as output:
             expected_data = "".join(read_to_unicode(output))
 
-        with patch("sys.stdin", new_callable=lambda: six.StringIO(test_data)):
+        if six.PY3:
+            f = lambda: six.StringIO(test_data)
+        else:
+            f = lambda: six.StringIO(test_data.encode('utf-8'))
+
+        with patch("sys.stdin", new_callable=f):
             main()
 
         html = mock_stdout.getvalue()

commit f31c7dd71f7cd0fd2f672ebe5362b3062fd43ffd

Use correct output encoding for --partial.
The problem was introduced in 506015955ade7a9cebfa2458b2ca31300c572e5e

ansi2html/converter.py
@@ -293,14 +293,14 @@ def main():
 
     # Produce only the headers and quit
     if opts.headers:
-        print(conv.produce_headers())
+        _print(conv.produce_headers())
         return
 
     # Process input line-by-line.  Produce no headers.
     if opts.partial or opts.inline:
         line = sys.stdin.readline()
         while line:
-            print(conv.convert(ansi=line, full=False)[:-1], end='\n')
+            _print(conv.convert(ansi=line, full=False)[:-1])
             line = sys.stdin.readline()
         return
 