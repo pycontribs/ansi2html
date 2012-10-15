b27349015a4944b98f019817a605b81b8df00180
Jens Timmerman <jens.timmerman@gmail.com>
2012-10-15 22:03:11 +0300

    Update ansi2html/style.py
    fixed typo

--- ansi2html/style.py
+++ ansi2html/style.py
@@ -51,8 +51,8 @@ def index2(grey):
 def get_styles(dark_bg=True):
 
     css = [
-        Rule('.body_foreground', color=('#000000', '#AAAAAA')[dark_bg])),
-        Rule('.body_background', background_color=('#AAAAAA', '#000000')[dark_bg])),
+        Rule('.body_foreground', color=('#000000', '#AAAAAA')[dark_bg]),
+        Rule('.body_background', background_color=('#AAAAAA', '#000000')[dark_bg]),
         Rule('.body_foreground > .bold,.bold > .body_foreground, body.body_foreground > pre > .bold',
              color=('#000000', '#FFFFFF')[dark_bg], font_weight=('bold', 'normal')[dark_bg]),
         Rule('.ansi1', font_weight='bold'),
