44e5e52faf6ea1eef57b8a3b1173f6794683dd4d
Sebastian Pipping <sebastian@pipping.org>
2013-09-28 00:27:12 +0200

    Add missing license headers

--- tests/test_ansi2html.py
+++ tests/test_ansi2html.py
@@ -1,3 +1,25 @@
+#  This file is part of ansi2html
+#  Convert ANSI (terminal) colours and attributes to HTML
+#  Copyright (C) 2012  Ralph Bean <rbean@redhat.com>
+#  Copyright (C) 2012  Kuno Woudt <kuno@frob.nl>
+#  Copyright (C) 2012  Martin Zimmermann <info@posativ.org>
+#  Copyright (C) 2013  Sebastian Pipping <sebastian@pipping.org>
+#
+#  Inspired by and developed off of the work by pixelbeat and blackjack.
+#
+#  This program is free software: you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License as
+#  published by the Free Software Foundation, either version 3 of
+#  the License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#  General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see
+#  <http://www.gnu.org/licenses/>.
 
 from os.path import abspath, dirname, join
 from ansi2html import Ansi2HTMLConverter
