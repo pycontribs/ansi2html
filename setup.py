86abc9e3dd8769af848a93ac2afc3728688554b3
Sebastian Pipping <sebastian@pipping.org>
2013-10-14 18:30:19 +0200

    Install man page to ${PREFIX}, not /usr (issue #39)

--- setup.py
+++ setup.py
@@ -89,7 +89,7 @@ setup(
     test_suite='nose.collector',
     packages=['ansi2html'],
     data_files=[
-        ('/usr/share/man/man1/', [
+        ('share/man/man1/', [
             'man/ansi2html.1',
             ]),
         ],
