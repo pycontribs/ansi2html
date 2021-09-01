8d47e4ed0fd7d4ef6ae19752d36e47959a3d4937
Sorin Sbarnea <sorin.sbarnea@gmail.com>
2021-09-01 10:46:22 +0100

    Bump setuptools-scm version (#138)

--- setup.py
+++ setup.py
@@ -11,5 +11,5 @@ site.ENABLE_USER_SITE = "--user" in sys.argv[1:]
 if __name__ == "__main__":
     setuptools.setup(
         use_scm_version={"local_scheme": "no-local-version"},
-        setup_requires=["setuptools_scm[toml]>=3.5.0"],
+        setup_requires=["setuptools_scm>=6.0.1"],
     )
