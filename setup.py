e0f975242f9bb775b411e697f3956556fca3025a
Daniel Ziegenberg <daniel@ziegenberg.at>
2021-10-25 16:56:58 +0200

    setup_requires is deprecated instead supplying build dependencies using PEP 517 pyproject.toml build-requires
    Signed-off-by: Daniel Ziegenberg <daniel@ziegenberg.at>

--- setup.py
+++ setup.py
@@ -11,5 +11,4 @@ site.ENABLE_USER_SITE = "--user" in sys.argv[1:]
 if __name__ == "__main__":
     setuptools.setup(
         use_scm_version={"local_scheme": "no-local-version"},
-        setup_requires=["setuptools_scm>=6.0.1"],
     )
