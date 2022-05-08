pycontribs/ansi2html

commit 331214b1faa9baec9367ce4e870a14e84a5b81ff

Add type hinting (#143)
Co-authored-by: Sorin Sbarnea <sorin.sbarnea@gmail.com>

ansi2html/converter.py
@@ -20,17 +20,17 @@
 #  along with this program.  If not, see
 #  <http://www.gnu.org/licenses/>.
 
-import io
 import optparse
 import re
 import sys
+from typing import Dict, Iterator, List, Optional, Set, Tuple, Union
 
 import pkg_resources
 
 try:
     from collections import OrderedDict
 except ImportError:
-    from ordereddict import OrderedDict
+    from ordereddict import OrderedDict  # type: ignore
 
 from ansi2html.style import SCHEME, get_styles
 
@@ -115,22 +115,22 @@ _html_template = """<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//E
 
 
 class _State:
-    def __init__(self):
+    def __init__(self) -> None:
         self.inside_span = False
         self.reset()
 
-    def reset(self):
-        self.intensity = ANSI_INTENSITY_NORMAL
-        self.style = ANSI_STYLE_NORMAL
-        self.blink = ANSI_BLINK_OFF
-        self.underline = ANSI_UNDERLINE_OFF
-        self.crossedout = ANSI_CROSSED_OUT_OFF
-        self.visibility = ANSI_VISIBILITY_ON
-        self.foreground = (ANSI_FOREGROUND_DEFAULT, None)
-        self.background = (ANSI_BACKGROUND_DEFAULT, None)
-        self.negative = ANSI_NEGATIVE_OFF
+    def reset(self) -> None:
+        self.intensity: int = ANSI_INTENSITY_NORMAL
+        self.style: int = ANSI_STYLE_NORMAL
+        self.blink: int = ANSI_BLINK_OFF
+        self.underline: int = ANSI_UNDERLINE_OFF
+        self.crossedout: int = ANSI_CROSSED_OUT_OFF
+        self.visibility: int = ANSI_VISIBILITY_ON
+        self.foreground: Tuple[int, Optional[int]] = (ANSI_FOREGROUND_DEFAULT, None)
+        self.background: Tuple[int, Optional[int]] = (ANSI_BACKGROUND_DEFAULT, None)
+        self.negative: int = ANSI_NEGATIVE_OFF
 
-    def adjust(self, ansi_code, parameter=None):
+    def adjust(self, ansi_code: int, parameter: Optional[int] = None) -> None:
         if ansi_code in (
             ANSI_INTENSITY_INCREASED,
             ANSI_INTENSITY_REDUCED,
@@ -174,17 +174,21 @@ class _State:
         elif ansi_code in (ANSI_NEGATIVE_ON, ANSI_NEGATIVE_OFF):
             self.negative = ansi_code
 
-    def to_css_classes(self):
-        css_classes = []
+    def to_css_classes(self) -> List[str]:
+        css_classes: List[str] = []
 
-        def append_unless_default(output, value, default):
+        def append_unless_default(output: List[str], value: int, default: int) -> None:
             if value != default:
                 css_class = "ansi%d" % value
                 output.append(css_class)
 
         def append_color_unless_default(
-            output, color, default, negative, neg_css_class
-        ):
+            output: List[str],
+            color: Tuple[int, Optional[int]],
+            default: int,
+            negative: bool,
+            neg_css_class: str,
+        ) -> None:
             value, parameter = color
             if value != default:
                 prefix = "inv" if negative else "ansi"
@@ -222,17 +226,17 @@ class _State:
 
 
 class OSC_Link:
-    def __init__(self, url, text):
+    def __init__(self, url: str, text: str) -> None:
         self.url = url
         self.text = text
 
 
-def map_vt100_box_code(char):
+def map_vt100_box_code(char: str) -> str:
     char_hex = hex(ord(char))
     return VT100_BOX_CODES[char_hex] if char_hex in VT100_BOX_CODES else char
 
 
-def _needs_extra_newline(text):
+def _needs_extra_newline(text: str) -> bool:
     if not text or text.endswith("\n"):
         return False
     return True
@@ -254,18 +258,18 @@ class Ansi2HTMLConverter:
 
     def __init__(
         self,
-        latex=False,
-        inline=False,
-        dark_bg=True,
-        line_wrap=True,
-        font_size="normal",
-        linkify=False,
-        escaped=True,
-        markup_lines=False,
-        output_encoding="utf-8",
-        scheme="ansi2html",
-        title="",
-    ):
+        latex: bool = False,
+        inline: bool = False,
+        dark_bg: bool = True,
+        line_wrap: bool = True,
+        font_size: str = "normal",
+        linkify: bool = False,
+        escaped: bool = True,
+        markup_lines: bool = False,
+        output_encoding: str = "utf-8",
+        scheme: str = "ansi2html",
+        title: str = "",
+    ) -> None:
 
         self.latex = latex
         self.inline = inline
@@ -278,7 +282,7 @@ class Ansi2HTMLConverter:
         self.output_encoding = output_encoding
         self.scheme = scheme
         self.title = title
-        self._attrs = None
+        self._attrs: Optional[Dict[str, Union[bool, str, Set[str]]]] = None
         self.hyperref = False
 
         if inline:
@@ -298,7 +302,7 @@ class Ansi2HTMLConverter:
         )
         self.osc_link_re = re.compile("\033\\]8;;(.*?)\007(.*?)\033\\]8;;\007")
 
-    def do_linkify(self, line):
+    def do_linkify(self, line: str) -> str:
         if not isinstance(line, str):
             return line  # If line is an object, e.g. OSC_Link, it
             # will be expanded to a string later
@@ -306,19 +310,19 @@ class Ansi2HTMLConverter:
             return self.url_matcher.sub(r"\\url{\1}", line)
         return self.url_matcher.sub(r'<a href="\1">\1</a>', line)
 
-    def handle_osc_links(self, part):
+    def handle_osc_links(self, part: OSC_Link) -> str:
         if self.latex:
             self.hyperref = True
             return """\\href{%s}{%s}""" % (part.url, part.text)
         return """<a href="%s">%s</a>""" % (part.url, part.text)
 
-    def apply_regex(self, ansi):
-        styles_used = set()
-        parts = self._apply_regex(ansi, styles_used)
-        parts = self._collapse_cursor(parts)
-        parts = list(parts)
+    def apply_regex(self, ansi: str) -> Tuple[str, Set[str]]:
+        styles_used: Set[str] = set()
+        all_parts = self._apply_regex(ansi, styles_used)
+        no_cursor_parts = self._collapse_cursor(all_parts)
+        no_cursor_parts = list(no_cursor_parts)
 
-        def _check_links(parts):
+        def _check_links(parts: List[Union[str, OSC_Link]]) -> Iterator[str]:
             for part in parts:
                 if isinstance(part, str):
                     if self.linkify:
@@ -330,7 +334,7 @@ class Ansi2HTMLConverter:
                 else:
                     yield part
 
-        parts = list(_check_links(parts))
+        parts = list(_check_links(no_cursor_parts))
         combined = "".join(parts)
         if self.markup_lines and not self.latex:
             combined = "\n".join(
@@ -341,7 +345,9 @@ class Ansi2HTMLConverter:
             )
         return combined, styles_used
 
-    def _apply_regex(self, ansi, styles_used):
+    def _apply_regex(
+        self, ansi: str, styles_used: Set[str]
+    ) -> Iterator[Union[str, OSC_Link, CursorMoveUp]]:
         if self.escaped:
             if (
                 self.latex
@@ -358,7 +364,7 @@ class Ansi2HTMLConverter:
             for pattern, special in specials.items():
                 ansi = ansi.replace(pattern, special)
 
-        def _vt100_box_drawing():
+        def _vt100_box_drawing() -> Iterator[str]:
             last_end = 0  # the index of the last end of a code we've seen
             box_drawing_mode = False
             for match in self.vt100_box_codes_prog.finditer(ansi):
@@ -374,7 +380,7 @@ class Ansi2HTMLConverter:
 
         ansi = "".join(_vt100_box_drawing())
 
-        def _osc_link(ansi):
+        def _osc_link(ansi: str) -> Iterator[Union[str, OSC_Link]]:
             last_end = 0
             for match in self.osc_link_re.finditer(ansi):
                 trailer = ansi[last_end : match.start()]
@@ -397,12 +403,15 @@ class Ansi2HTMLConverter:
             else:
                 yield "</span>"
 
-    def _handle_ansi_code(self, ansi, styles_used, state):
+    def _handle_ansi_code(
+        self, ansi: str, styles_used: Set[str], state: _State
+    ) -> Iterator[Union[str, CursorMoveUp]]:
         last_end = 0  # the index of the last end of a code we've seen
         for match in self.ansi_codes_prog.finditer(ansi):
             yield ansi[last_end : match.start()]
             last_end = match.end()
 
+            params: Union[str, List[int]]
             params, command = match.groups()
 
             if command not in "mMA":
@@ -410,7 +419,7 @@ class Ansi2HTMLConverter:
 
             # Special cursor-moving code.  The only supported one.
             if command == "A":
-                yield CursorMoveUp
+                yield CursorMoveUp  # type: ignore
                 continue
 
             try:
@@ -452,7 +461,7 @@ class Ansi2HTMLConverter:
 
                 if v in (ANSI_FOREGROUND_256, ANSI_BACKGROUND_256):
                     try:
-                        parameter = params[i + 2]
+                        parameter: Optional[int] = params[i + 2]
                     except IndexError:
                         continue
                     skip_after_index = i + 2
@@ -495,10 +504,12 @@ class Ansi2HTMLConverter:
             state.inside_span = True
         yield ansi[last_end:]
 
-    def _collapse_cursor(self, parts):
+    def _collapse_cursor(
+        self, parts: Iterator[Union[str, OSC_Link, CursorMoveUp]]
+    ) -> List[Union[str, OSC_Link]]:
         """Act on any CursorMoveUp commands by deleting preceding tokens"""
 
-        final_parts = []
+        final_parts: List[Union[str, OSC_Link]] = []
         for part in parts:
 
             # Throw out empty string tokens ("")
@@ -506,11 +517,11 @@ class Ansi2HTMLConverter:
                 continue
 
             # Go back, deleting every token in the last 'line'
-            if part == CursorMoveUp:
+            if isinstance(part, CursorMoveUp):
                 if final_parts:
                     final_parts.pop()
 
-                while final_parts and "\n" not in final_parts[-1]:
+                while final_parts and "\n" not in final_parts[-1]:  # type: ignore
                     final_parts.pop()
 
                 continue
@@ -520,7 +531,9 @@ class Ansi2HTMLConverter:
 
         return final_parts
 
-    def prepare(self, ansi="", ensure_trailing_newline=False):
+    def prepare(
+        self, ansi: str = "", ensure_trailing_newline: bool = False
+    ) -> Dict[str, Union[bool, str, Set[str]]]:
         """Load the contents of 'ansi' into this object"""
 
         body, styles = self.apply_regex(ansi)
@@ -538,13 +551,15 @@ class Ansi2HTMLConverter:
 
         return self._attrs
 
-    def attrs(self):
+    def attrs(self) -> Dict[str, Union[bool, str, Set[str]]]:
         """Prepare attributes for the template"""
         if not self._attrs:
             raise Exception("Method .prepare not yet called.")
         return self._attrs
 
-    def convert(self, ansi, full=True, ensure_trailing_newline=False):
+    def convert(
+        self, ansi: str, full: bool = True, ensure_trailing_newline: bool = False
+    ) -> str:
         r"""
         :param ansi: ANSI sequence to convert.
         :param full: Whether to include the full HTML document or only the body.
@@ -552,7 +567,7 @@ class Ansi2HTMLConverter:
         """
         attrs = self.prepare(ansi, ensure_trailing_newline=ensure_trailing_newline)
         if not full:
-            return attrs["body"]
+            return attrs["body"]  # type: ignore
         if self.latex:
             _template = _latex_template
         else:
@@ -560,7 +575,7 @@ class Ansi2HTMLConverter:
         all_styles = get_styles(self.dark_bg, self.line_wrap, self.scheme)
         backgrounds = all_styles[:6]
         used_styles = filter(
-            lambda e: e.klass.lstrip(".") in attrs["styles"], all_styles
+            lambda e: e.klass.lstrip(".") in attrs["styles"], all_styles  # type: ignore
         )
 
         return _template % {
@@ -572,7 +587,7 @@ class Ansi2HTMLConverter:
             "hyperref": "\\usepackage{hyperref}" if self.hyperref else "",
         }
 
-    def produce_headers(self):
+    def produce_headers(self) -> str:
         return '<style type="text/css">\n%(style)s\n</style>\n' % {
             "style": "\n".join(
                 map(str, get_styles(self.dark_bg, self.line_wrap, self.scheme))
@@ -580,7 +595,7 @@ class Ansi2HTMLConverter:
         }
 
 
-def main():
+def main() -> None:
     """
     $ ls --color=always | ansi2html > directories.html
     $ sudo tail /var/log/messages | ccze -A | ansi2html > logs.html
@@ -718,16 +733,7 @@ def main():
         title=opts.output_title,
     )
 
-    try:
-        sys.stdin = io.TextIOWrapper(sys.stdin.detach(), opts.input_encoding, "replace")
-    except io.UnsupportedOperation:
-        # This only fails in the test suite...
-        pass
-
-    def _read(input_bytes):
-        return input_bytes
-
-    def _print(output_unicode, end="\n"):
+    def _print(output_unicode: str, end: str = "\n") -> None:
         if hasattr(sys.stdout, "buffer"):
             output_bytes = (output_unicode + end).encode(opts.output_encoding)
             sys.stdout.buffer.write(output_bytes)

.pre-commit-config.yaml
@@ -37,3 +37,19 @@ repos:
         additional_dependencies:
           - ansible-base
           - testinfra
+  - repo: https://github.com/pre-commit/mirrors-mypy
+    rev: v0.910-1
+    hooks:
+    - id: mypy
+      # empty args needed in order to match mypy cli behavior
+      args: ["--strict"]
+      additional_dependencies:
+      - pytest
+      - types-setuptools
+      - types-pkg_resources
+      - types-mock
+      exclude: >
+        (?x)^(
+          docs/.*|
+          setup.py
+        )$

ansi2html/style.py
@@ -17,8 +17,11 @@
 #    <http://www.gnu.org/licenses/>.
 
 
+from typing import List
+
+
 class Rule:
-    def __init__(self, klass, **kw):
+    def __init__(self, klass: str, **kw: str) -> None:
 
         self.klass = klass
         self.kw = "; ".join(
@@ -26,21 +29,21 @@ class Rule:
         ).strip()
         self.kwl = [(k.replace("_", "-"), kw[k][1:]) for k in sorted(kw.keys())]
 
-    def __str__(self):
+    def __str__(self) -> str:
         return "%s { %s; }" % (self.klass, self.kw)
 
 
-def index(r, g, b):
+def index(r: int, g: int, b: int) -> str:
     return str(16 + (r * 36) + (g * 6) + b)
 
 
-def color_component(x):
+def color_component(x: int) -> int:
     if x == 0:
         return 0
     return 0x37 + (0x28 * x)
 
 
-def color(r, g, b):
+def color(r: int, g: int, b: int) -> str:
     return "#%.2x%.2x%.2x" % (
         color_component(r),
         color_component(g),
@@ -48,11 +51,11 @@ def color(r, g, b):
     )
 
 
-def level(grey):
+def level(grey: int) -> str:
     return "#%.2x%.2x%.2x" % (((grey * 10) + 8,) * 3)
 
 
-def index2(grey):
+def index2(grey: int) -> str:
     return str(232 + grey)
 
 
@@ -164,14 +167,16 @@ SCHEME = {
 }
 
 
-def intensify(color, dark_bg, amount=64):
+def intensify(color: str, dark_bg: bool, amount: int = 64) -> str:
     if not dark_bg:
         amount = -amount
     rgb = tuple(max(0, min(255, amount + int(color[i : i + 2], 16))) for i in (1, 3, 5))
     return "#%.2x%.2x%.2x" % rgb
 
 
-def get_styles(dark_bg=True, line_wrap=True, scheme="ansi2html"):
+def get_styles(
+    dark_bg: bool = True, line_wrap: bool = True, scheme: str = "ansi2html"
+) -> List[Rule]:
     css = [
         Rule(
             ".ansi2html-content",

ansi2html/util.py
@@ -1,2 +1,5 @@
-def read_to_unicode(obj):
+from typing import BinaryIO, List
+
+
+def read_to_unicode(obj: BinaryIO) -> List[str]:
     return [line.decode("utf-8") for line in obj.readlines()]

mypy.ini
@@ -1,0 +1,3 @@
+[mypy]
+warn_return_any = True
+warn_unused_configs = True

tests/test_ansi2html.py
@@ -26,6 +26,7 @@ import unittest
 from io import StringIO
 from os.path import abspath, dirname, join
 from subprocess import run
+from typing import List
 
 from mock import patch
 
@@ -51,31 +52,31 @@ _here = dirname(abspath(__file__))
 class TestAnsi2HTML(unittest.TestCase):
     maxDiff = None
 
-    def test_linkify(self):
+    def test_linkify(self) -> None:
         ansi = "http://threebean.org"
         target = '<a href="http://threebean.org">http://threebean.org</a>'
         html = Ansi2HTMLConverter(linkify=True).convert(ansi)
         assert target in html
 
-    def test_not_linkify(self):
+    def test_not_linkify(self) -> None:
         ansi = "http://threebean.org"
         target = '<a href="http://threebean.org">http://threebean.org</a>'
         html = Ansi2HTMLConverter().convert(ansi)
         assert target not in html
 
-    def test_osc_link(self):
+    def test_osc_link(self) -> None:
         ansi = "[\x1b[01;35m\x1b[K\x1b]8;;https://gcc.gnu.org/onlinedocs/gcc/Warning-Options.html#index-Wtype-limits\x07-Wtype-limits\x1b]8;;\x07\x1b[m\x1b[K]\n"
         target = '[<span class="ansi1 ansi35"><a href="https://gcc.gnu.org/onlinedocs/gcc/Warning-Options.html#index-Wtype-limits">-Wtype-limits</a></span>]'
         html = Ansi2HTMLConverter().convert(ansi)
         assert target in html
 
-    def test_osc_link_latex(self):
+    def test_osc_link_latex(self) -> None:
         ansi = "[\x1b[01;35m\x1b[K\x1b]8;;https://gcc.gnu.org/onlinedocs/gcc/Warning-Options.html#index-Wtype-limits\x07-Wtype-limits\x1b]8;;\x07\x1b[m\x1b[K]\n"
         target = "[\\textcolor{ansi1 ansi35}{\\href{https://gcc.gnu.org/onlinedocs/gcc/Warning-Options.html#index-Wtype-limits}{-Wtype-limits}}]"
         html = Ansi2HTMLConverter(latex=True).convert(ansi)
         assert target in html
 
-    def test_conversion(self):
+    def test_conversion(self) -> None:
         for input_filename, expected_output_filename in (
             ("ansicolor.txt", "ansicolor.html"),
             ("ansicolor_eix.txt", "ansicolor_eix.html"),
@@ -98,14 +99,16 @@ class TestAnsi2HTML(unittest.TestCase):
 
     @patch("sys.argv", new_callable=lambda: ["ansi2html"])
     @patch("sys.stdout", new_callable=StringIO)
-    def test_conversion_as_command(self, mock_stdout, mock_argv):
+    def test_conversion_as_command(
+        self, mock_stdout: StringIO, mock_argv: List[str]
+    ) -> None:
         with open(join(_here, "ansicolor.txt"), "rb") as input:
             test_data = "".join(read_to_unicode(input))
 
         with open(join(_here, "ansicolor.html"), "rb") as output:
             expected_data = "".join(read_to_unicode(output))
 
-        def f():
+        def f() -> StringIO:
             return StringIO(test_data)
 
         with patch("sys.stdin", new_callable=f):
@@ -115,7 +118,7 @@ class TestAnsi2HTML(unittest.TestCase):
 
         self.assertEqual(html, expected_data)
 
-    def test_unicode(self):
+    def test_unicode(self) -> None:
         """Ensure that the converter returns unicode(py2)/str(py3) objs."""
 
         with open(join(_here, "ansicolor.txt"), "rb") as input:
@@ -128,7 +131,9 @@ class TestAnsi2HTML(unittest.TestCase):
 
     @patch("sys.argv", new_callable=lambda: ["ansi2html", "--inline"])
     @patch("sys.stdout", new_callable=StringIO)
-    def test_inline_as_command(self, mock_stdout, mock_argv):
+    def test_inline_as_command(
+        self, mock_stdout: StringIO, mock_argv: List[str]
+    ) -> None:
         test_input = textwrap.dedent(
             """
         this is
@@ -144,15 +149,14 @@ class TestAnsi2HTML(unittest.TestCase):
 
     @patch("sys.argv", new_callable=lambda: ["ansi2html", "--partial"])
     @patch("sys.stdout", new_callable=StringIO)
-    def test_partial_as_command(self, mock_stdout, mock_argv):
+    def test_partial_as_command(
+        self, mock_stdout: StringIO, mock_argv: List[str]
+    ) -> None:
         rainbow = "\x1b[1m\x1b[40m\x1b[31mr\x1b[32ma\x1b[33mi\x1b[34mn\x1b[35mb\x1b[36mo\x1b[37mw\x1b[0m\n"
         with patch("sys.stdin", new_callable=lambda: StringIO(rainbow)):
             main()
 
         html = mock_stdout.getvalue().strip()
-
-        if hasattr(html, "decode"):
-            html = html.decode("utf-8")
 
         expected = (
             '<span class="ansi1"></span>'
@@ -169,7 +173,7 @@ class TestAnsi2HTML(unittest.TestCase):
         assert isinstance(expected, str)
         self.assertEqual(expected, html)
 
-    def test_partial(self):
+    def test_partial(self) -> None:
         rainbow = "\x1b[1m\x1b[40m\x1b[31mr\x1b[32ma\x1b[33mi\x1b[34mn\x1b[35mb\x1b[36mo\x1b[37mw\x1b[0m\n"
 
         html = Ansi2HTMLConverter().convert(rainbow, full=False).strip()
@@ -186,7 +190,7 @@ class TestAnsi2HTML(unittest.TestCase):
         )
         self.assertEqual(expected, html)
 
-    def test_inline(self):
+    def test_inline(self) -> None:
 
         rainbow = "\x1b[1m\x1b[40m\x1b[31mr\x1b[32ma\x1b[33mi\x1b[34mn\x1b[35mb\x1b[36mo\x1b[37mw\x1b[0m"
 
@@ -205,7 +209,7 @@ class TestAnsi2HTML(unittest.TestCase):
 
         self.assertEqual(expected, html)
 
-    def test_produce_headers(self):
+    def test_produce_headers(self) -> None:
         conv = Ansi2HTMLConverter()
         headers = conv.produce_headers()
 
@@ -215,37 +219,37 @@ class TestAnsi2HTML(unittest.TestCase):
 
         self.assertMultiLineEqual(headers, "".join(expected_data))
 
-    def test_escaped_implicit(self):
+    def test_escaped_implicit(self) -> None:
         test = "<p>awesome</p>"
         expected = "&lt;p&gt;awesome&lt;/p&gt;"
         html = Ansi2HTMLConverter().convert(test, full=False)
         self.assertEqual(expected, html)
 
-    def test_escaped_explicit(self):
+    def test_escaped_explicit(self) -> None:
         test = "<p>awesome</p>"
         expected = "&lt;p&gt;awesome&lt;/p&gt;"
         html = Ansi2HTMLConverter(escaped=True).convert(test, full=False)
         self.assertEqual(expected, html)
 
-    def test_unescaped(self):
+    def test_unescaped(self) -> None:
         test = "<p>awesome</p>"
         expected = "<p>awesome</p>"
         html = Ansi2HTMLConverter(escaped=False).convert(test, full=False)
         self.assertEqual(expected, html)
 
-    def test_markup_lines(self):
+    def test_markup_lines(self) -> None:
         test = "  wat  \n "
         expected = '<span id="line-0">  wat  </span>\n<span id="line-1"> </span>'
         html = Ansi2HTMLConverter(markup_lines=True).convert(test, full=False)
         self.assertEqual(expected, html)
 
-    def test_no_markup_lines(self):
+    def test_no_markup_lines(self) -> None:
         test = "  wat  \n "
         expected = test
         html = Ansi2HTMLConverter().convert(test, full=False)
         self.assertEqual(expected, html)
 
-    def test_issue_25(self):
+    def test_issue_25(self) -> None:
         sample = "\x1b[0;38;5;238;48;5;231mTEXT\x1b[0m"
 
         html = Ansi2HTMLConverter(inline=False).convert(sample, full=False)
@@ -253,7 +257,7 @@ class TestAnsi2HTML(unittest.TestCase):
 
         self.assertEqual(expected, html)
 
-    def test_italic(self):
+    def test_italic(self) -> None:
         sample = "\x1b[3mITALIC\x1b[0m"
 
         html = Ansi2HTMLConverter(inline=True).convert(sample, full=False)
@@ -261,7 +265,7 @@ class TestAnsi2HTML(unittest.TestCase):
 
         self.assertEqual(expected, html)
 
-    def test_hidden_text(self):
+    def test_hidden_text(self) -> None:
         sample = "\x1b[%dmHIDDEN\x1b[%dmVISIBLE\x1b[0m" % (
             ANSI_VISIBILITY_OFF,
             ANSI_VISIBILITY_ON,
@@ -272,7 +276,7 @@ class TestAnsi2HTML(unittest.TestCase):
 
         self.assertEqual(expected, html)
 
-    def test_lighter_text(self):
+    def test_lighter_text(self) -> None:
         sample = "NORMAL\x1b[%dmLIGHTER\x1b[%dmBOLD\x1b[%dmNORMAL" % (
             ANSI_INTENSITY_REDUCED,
             ANSI_INTENSITY_INCREASED,
@@ -284,7 +288,7 @@ class TestAnsi2HTML(unittest.TestCase):
 
         self.assertEqual(expected, html)
 
-    def test_blinking_text(self):
+    def test_blinking_text(self) -> None:
         sample = "\x1b[%dm555\x1b[%dm666\x1b[%dmNOBLINK\x1b[0m" % (
             ANSI_BLINK_SLOW,
             ANSI_BLINK_FAST,
@@ -299,7 +303,7 @@ class TestAnsi2HTML(unittest.TestCase):
         expected = '<span class="ansi5">555</span><span class="ansi6">666</span>NOBLINK'
         self.assertEqual(expected, html)
 
-    def test_inverse_text(self):
+    def test_inverse_text(self) -> None:
         sample = "NORMAL\x1b[%dmINVERSE\x1b[%dmNORMAL\x1b[0m" % (
             ANSI_NEGATIVE_ON,
             ANSI_NEGATIVE_OFF,
@@ -327,7 +331,7 @@ class TestAnsi2HTML(unittest.TestCase):
         expected = 'NORMAL<span class="ansi31">313131</span><span class="inv31 inv_foreground">!31!31!31</span><span class="inv31 inv43">!31!43</span><span class="ansi31 ansi43">31+43</span>NORMAL'
         self.assertEqual(expected, html)
 
-    def test_cross_line_state(self):  # covers issue 36, too
+    def test_cross_line_state(self) -> None:  # covers issue 36, too
         sample = "\x1b[31mRED\nSTILL RED"
         html = Ansi2HTMLConverter(inline=True).convert(
             sample, full=False, ensure_trailing_newline=False
@@ -363,7 +367,7 @@ class TestAnsi2HTML(unittest.TestCase):
         expected = '<span style="color: #aa0000">RED\nSTILL RED</span>\n'
         self.assertEqual(expected, html)
 
-    def test_scheme(self):  # covers issue 36, too
+    def test_scheme(self) -> None:  # covers issue 36, too
         sample = "\x1b[33mYELLOW/BROWN"
         # ansi2html scheme is brown #aa5500
         html = Ansi2HTMLConverter(inline=True).convert(
@@ -379,30 +383,30 @@ class TestAnsi2HTML(unittest.TestCase):
         expected = '<span style="color: #cdcd00">YELLOW/BROWN</span>'
         self.assertEqual(expected, html)
 
-    def test_latex_inline(self):
+    def test_latex_inline(self) -> None:
         ansi = "\x1b[33mYELLOW/BROWN"
         target = "\\textcolor[HTML]{aa5500}{YELLOW/BROWN}"
         latex = Ansi2HTMLConverter(latex=True, inline=True).convert(ansi)
         assert target in latex
 
-    def test_latex_title(self):
+    def test_latex_title(self) -> None:
         ansi = ""
         title = "testing"
         target = "\\title{%s}" % title
         latex = Ansi2HTMLConverter(latex=True, inline=True, title=title).convert(ansi)
         assert target in latex
 
-    def test_latex_linkify(self):
+    def test_latex_linkify(self) -> None:
         ansi = "http://python.org/"
         target = "\\url{%s}" % ansi
         latex = Ansi2HTMLConverter(latex=True, inline=True, linkify=True).convert(ansi)
         assert target in latex
 
-    def test_command_script(self):
+    def test_command_script(self) -> None:
         result = run(["ansi2html", "--version"], check=True)
         assert result.returncode == 0
 
-    def test_command_module(self):
+    def test_command_module(self) -> None:
         result = run(["python3", "-m", "ansi2html", "--version"], check=True)
         assert result.returncode == 0
 

commit 3931e8ca96b6857f3714870eada5251e78b2e0f4

Restore --input-encoding functionality
Regression from commit 331214b1faa9baec9367ce4e870a14e84a5b81ff

ansi2html/converter.py
@@ -20,6 +20,7 @@
 #  along with this program.  If not, see
 #  <http://www.gnu.org/licenses/>.
 
+import io
 import optparse
 import re
 import sys
@@ -785,6 +786,12 @@ def main() -> None:
         scheme=opts.scheme,
         title=opts.output_title,
     )
+
+    if hasattr(sys.stdin, "detach") and not isinstance(
+        sys.stdin, io.StringIO
+    ):  # e.g. during tests
+        input_buffer = sys.stdin.detach()  # type: ignore
+        sys.stdin = io.TextIOWrapper(input_buffer, opts.input_encoding, "replace")
 
     def _print(output_unicode: str, end: str = "\n") -> None:
         if hasattr(sys.stdout, "buffer"):