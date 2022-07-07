b7821e5e6015df258657f085e31014eeceb80e51
Jayson Larose <jayson@interlaced.org>
2022-07-07 04:08:08 -0700

    Added fix for ANSI color codes that include blank values (e.g., wezterm's new `get_selection_escapes_for_pane()` method).

--- ansi2html/converter.py
+++ ansi2html/converter.py
@@ -455,7 +455,7 @@ class Ansi2HTMLConverter:
                 continue
 
             try:
-                params = [int(x) for x in re.split("[;:]", params)]
+                params = [int(x) for x in re.split("[;:]", params) if len(x) > 0]
             except ValueError:
                 params = [ANSI_FULL_RESET]
 
